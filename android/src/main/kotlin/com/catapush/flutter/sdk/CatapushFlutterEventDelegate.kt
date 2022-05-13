f0a82fe7a30d873925415637d39d51550edd69af
Mattia Aracne <mattia@codeatlas.it>
2022-05-13 16:39:20 +0200

    Update plugin to Catapush SDK for Android 12.0.0

--- android/src/main/kotlin/com/catapush/flutter/sdk/CatapushFlutterEventDelegate.kt
+++ android/src/main/kotlin/com/catapush/flutter/sdk/CatapushFlutterEventDelegate.kt
@@ -1,54 +1,56 @@
 package com.catapush.flutter.sdk
 
 import android.content.Context
-import com.catapush.library.CatapushTwoWayReceiver
 import com.catapush.library.exceptions.CatapushAuthenticationError
+import com.catapush.library.exceptions.CatapushConnectionError
 import com.catapush.library.exceptions.PushServicesException
+import com.catapush.library.interfaces.ICatapushEventDelegate
 import com.catapush.library.messages.CatapushMessage
 import com.catapush.library.push.models.PushPlatformType
 import com.google.android.gms.common.GoogleApiAvailability
+import java.lang.ref.WeakReference
 import java.lang.reflect.Modifier
 
-class CatapushFlutterReceiver: CatapushTwoWayReceiver() {
+object CatapushFlutterEventDelegate: ICatapushEventDelegate {
 
-    companion object {
-        private var messagesDispatcher: IMessagesDispatchDelegate? = null
-        private var statusDispatcher: IStatusDispatchDelegate? = null
+    private var contextRef: WeakReference<Context>? = null
+    private var messagesDispatcher: IMessagesDispatchDelegate? = null
+    private var statusDispatcher: IStatusDispatchDelegate? = null
 
-        fun setMessagesDispatcher(messagesDispatcher: IMessagesDispatchDelegate) {
-            Companion.messagesDispatcher = messagesDispatcher
-        }
-
-        fun setStatusDispatcher(statusDispatcher: IStatusDispatchDelegate) {
-            Companion.statusDispatcher = statusDispatcher
-        }
+    fun setContext(context: Context) {
+        this.contextRef = WeakReference(context)
     }
 
-    override fun onDisconnected(errorCode: Int, context: Context) {
+    fun setMessagesDispatcher(messagesDispatcher: IMessagesDispatchDelegate) {
+        this.messagesDispatcher = messagesDispatcher
+    }
+
+    fun setStatusDispatcher(statusDispatcher: IStatusDispatchDelegate) {
+        this.statusDispatcher = statusDispatcher
+    }
+
+    override fun onDisconnected(error: CatapushConnectionError) {
         statusDispatcher?.dispatchConnectionStatus("disconnected")
     }
 
-    override fun onMessageOpened(message: CatapushMessage, context: Context) {
-        // TODO
+    override fun onMessageOpened(message: CatapushMessage) {
     }
 
-    override fun onMessageOpenedConfirmed(message: CatapushMessage, context: Context) {
-        // TODO
+    override fun onMessageOpenedConfirmed(message: CatapushMessage) {
     }
 
-    override fun onMessageSent(message: CatapushMessage, context: Context) {
+    override fun onMessageSent(message: CatapushMessage) {
         messagesDispatcher?.dispatchMessageSent(message)
     }
 
-    override fun onMessageSentConfirmed(message: CatapushMessage, context: Context) {
-        // TODO
+    override fun onMessageSentConfirmed(message: CatapushMessage) {
     }
 
-    override fun onMessageReceived(message: CatapushMessage, context: Context) {
+    override fun onMessageReceived(message: CatapushMessage) {
         messagesDispatcher?.dispatchMessageReceived(message)
     }
 
-    override fun onRegistrationFailed(error: CatapushAuthenticationError, context: Context) {
+    override fun onRegistrationFailed(error: CatapushAuthenticationError) {
         CatapushAuthenticationError::class.java.declaredFields.firstOrNull {
             Modifier.isStatic(it.modifiers)
                     && it.type == Integer::class
@@ -56,23 +58,24 @@ class CatapushFlutterReceiver: CatapushTwoWayReceiver() {
         }?.also { statusDispatcher?.dispatchError(it.name, error.reasonCode) }
     }
 
-    override fun onConnecting(context: Context) {
+    override fun onConnecting() {
         statusDispatcher?.dispatchConnectionStatus("connecting")
     }
 
-    override fun onConnected(context: Context) {
+    override fun onConnected() {
         statusDispatcher?.dispatchConnectionStatus("connected")
     }
 
-    override fun onPushServicesError(e: PushServicesException, context: Context) {
-        // TODO
+    override fun onPushServicesError(e: PushServicesException) {
         if (PushPlatformType.GMS.name == e.platform && e.isUserResolvable) {
             // It's a GMS error and it's user resolvable: show a notification to the user
             val gmsAvailability = GoogleApiAvailability.getInstance()
             /*gmsAvailability.setDefaultNotificationChannelId(
                 context, brandSupport.getNotificationChannelId(context)
             )*/
-            gmsAvailability.showErrorNotification(context, e.errorCode)
+            contextRef?.get()?.also {
+                gmsAvailability.showErrorNotification(it, e.errorCode)
+            }
         }
     }
 
